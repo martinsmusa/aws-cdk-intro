9783d80e1f3bca3923cb69a47ebe528dcdd019e6
Martins <martins.musa90@gmail.com>
2023-12-10 18:06:44 +0200

    Section 3

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStarterStack } from '../lib/cdk-starter-stack';
+import {PhotosStack} from "../lib/photos-stack";
+import {PhotosHandlerStack} from "../lib/photos-handler-stack";
+import {BucketTagger} from "./Tagger";
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack');
+// new CdkStarterStack(app, 'CdkStarterStack');
+const photosStack = new PhotosStack(app, 'PhotosStack');
+
+new PhotosHandlerStack(app, 'PhotosHandlerStack', {
+    targetBucketArn: photosStack.photosBucketArn
+});
+
+const tagger = new BucketTagger('level', 'test');
+
+cdk.Aspects.of(app).add(tagger);
